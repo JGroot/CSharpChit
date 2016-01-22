2575d6d9e1682c898f5904dcccf0540f9f973bdf
Groot <jg105@zips.uakron.edu>
2016-01-21 19:39:41 -0500

    a little list

--- RectAndJaggedArray/Program.cs
+++ RectAndJaggedArray/Program.cs
@@ -12,36 +12,40 @@ namespace RectAndJaggedArray
             int[,] rectangular = { { 1, 2, 3 }, { 4, 5, 6 } };
             int[][] jagged = 
             {
-               new int[] {1, 2},
+               new int[] {1,2},
                new int[] {3},
-               new int[] {4, 5, 6}
+               new int[] {4,5,6}
             };
 
             OutputArray(rectangular);
             Console.WriteLine();
             OutputArray(jagged);
-       
 
+            Console.WriteLine("Element: ", jagged[0][0].ToString());
         }
 
         private static void OutputArray(int[,] array)
         {
             Console.WriteLine("Values in the rectangular array by row are");
-            for (int row = 0; row < array.GetLength(0); ++row)
-                for (int column = 0; column < array.GetLength(1); ++column)
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int column = 0; column < array.GetLength(1); column++)
                     Console.WriteLine("{0} ", array[row, column]);
 
-            Console.WriteLine();
+                Console.WriteLine();
+            }
         }
 
         private static void OutputArray(int[][] array)
         {
             Console.WriteLine("Values in the jagged array by row are");
             foreach (int[] row in array)
+            {
                 foreach (int[] column in array)
                     Console.WriteLine("{0} ", column);
 
-            Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ TestAverage/TestAverage/CSList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace CSList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = new List<string>();
+
+            items.Add("red");
+            items.Add("blue");
+            items.Add("green");
+            items.Insert(0, "purple");
+
+            foreach (string item in items)
+                Console.WriteLine(item);
+        }
+    }
+}
