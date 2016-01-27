a4b895c0686e6eee1632c3040137359e0defe64e
Groot <jg105@zips.uakron.edu>
2016-01-26 19:56:59 -0500

    Enumerations (dice game)

--- TestAverage/TestAverage/StrModConcat/Program.cs
+++ TestAverage/TestAverage/StrModConcat/Program.cs
@@ -14,6 +14,17 @@ namespace StrModConcat
             string str2 = "Birthday";
 
             Console.WriteLine(str1 + str2);
+            Console.WriteLine(string.Concat(str1, str2));
+
+            string str3 = "Cheers";
+            string str4 = "GOOD BYE";
+            string str5 = "     spaces      ";
+
+            Console.WriteLine(str3.Replace('e', 'E'));
+            Console.WriteLine(str3.ToUpper());
+            Console.WriteLine(str5.Trim());
+            Console.WriteLine(str4.ToLower());
+
         }
     }
 }
