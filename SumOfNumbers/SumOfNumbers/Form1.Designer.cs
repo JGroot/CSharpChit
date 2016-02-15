31579d8bb4533fa60572d633f6ce2976f39a2020
JGroot <gesikamarie@gmail.com>
2016-02-14 21:41:29 -0500

    Sum of Number program

---
+++ SumOfNumbers/SumOfNumbers/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SumOfNumbers
+{
+    partial class frmCalculate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(26, 39);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.Size = new System.Drawing.Size(316, 20);
+            this.txtNumbers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "=";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(367, 42);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(13, 13);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "?";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(414, 37);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter numbers seperated by a comma below:";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblError.Location = new System.Drawing.Point(26, 65);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 5;
+            // 
+            // frmCalculate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 113);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumbers);
+            this.Name = "frmCalculate";
+            this.Text = "Calculate ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumbers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ SumOfNumbers/SumOfNumbers/Form1.cs
@@ -0,0 +1,56 @@
+﻿//  Sum of Numbers in a String 
+//  Create an application that lets the user enter a string containing 
+//  series of numbers separated by commas. Here is an example of valid input: 
+//      7, 9, 10, 2, 18, 6 
+//  The program should calculate and display the sum of all the numbers.
+//Author:  Jessica Groot
+//Date: 02/10/2016
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SumOfNumbers
+{
+    public partial class frmCalculate : Form
+    {
+        public frmCalculate()
+        {
+            InitializeComponent();
+        }
+    
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string input = txtNumbers.Text;
+            string[] numbers = { };
+            decimal total = 0;
+            string error = string.Empty;
+
+            if (input != string.Empty)
+            {
+                numbers = Regex.Split(input, "\\s?,\\s?");
+                foreach (string number in numbers)
+                {
+                    if (Regex.IsMatch(number, "^\\d*\\.?\\d+?$"))
+                        total += Convert.ToDecimal(number);
+                    else
+                        error += "\'"+ number +"\'" + "is not a valid number." + Environment.NewLine;
+                }
+            }
+            lblAnswer.Text = total.ToString();
+            if (error != string.Empty)
+                lblError.Text = error; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
