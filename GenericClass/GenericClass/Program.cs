23e9f05f50979ed32b3385c174f62182a0e56185
jgroot <gesikamarie@gmail.com>
2016-03-03 19:54:40 -0500

    Generic stuff

---
+++ GenericClass/GenericClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static double[] doubleElements = new double[] {1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+        private static int[] intElements = new int[] {1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11};
+
+        private static Stack<double> doubleStack;
+        private static Stack<int> intStack;
+
+        static void Main(string[] args)
+        {
+            doubleStack = new Stack<double>(5);
+            intStack = new Stack<int>(10);
+
+            TestPushDouble();
+            TestPopDoubel();
+            TestPushInt();
+            TestPopInt();
+        }
+    }
+}
---
+++ GenericClass/GenericClass/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Stack<T>
+    {
+        private int top;
+        private T[] elements;
+
+        public Stack():this(10)
+        {
+        }
+
+        public Stack(int stackSize)
+        {
+            if (stackSize > 0)
+                elements = new T[stackSize];
+            else
+                throw new ArgumentException("Stack size must be positive.");
+
+            top = -1;
+        }
+
+        public void Push(T pushValue)
+        {
+            if (top == elements.Length - 1)
+                throw new FullStackException(string.Format(
+                    "Stack is full, cannot push {0}", pushValue));
+
+            ++top;
+            elements[top] = pushValue;
+        }
+
+        public T Pop()
+        {
+            if (top == -1)
+                throw new EmptyStackException("Stack is empty, cannot pop");
+            --top;
+            return elements[top + 1];
+        }     
+    }
+}
