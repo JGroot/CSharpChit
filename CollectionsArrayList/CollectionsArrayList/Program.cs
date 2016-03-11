b6c64f4dbffeba5af882f2408f00b916fc2cf724
Jessica Groot <jg105@zips.uakron.edu>
2016-03-10 19:18:07 -0500

    Array Lists, weeeeee

---
+++ CollectionsArrayList/CollectionsArrayList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsArrayList
+{
+    class Program
+    {
+        private static readonly string[] colors = { "MAGENTA", "WHITE", "BLUE", "RED", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList(1);
+            foreach (var color in colors)
+                list.Add(color);
+
+            ArrayList removeList = new ArrayList(removeColors);
+
+            Console.Write("ArrayList: ");
+            DisplayInformation(list);
+
+            RemoveColors(list, removeList);
+
+            Console.Write("\n ArrayList after calling RemoveColors(): ");
+
+            DisplayInformation(list);
+        }
+
+        private static void DisplayInformation(ArrayList arrayList)
+        {
+            foreach (var element in arrayList)
+                Console.Write(element + " ");
+            Console.WriteLine("\n Size = " + arrayList.Count + "; Capacity = " + arrayList.Capacity);
+
+            int index = arrayList.IndexOf("BLUE");
+
+            if (index != -1)
+                Console.WriteLine("The arraylist contains Blue at index " + index);
+            else
+                Console.WriteLine("The arraylist does not contain Blue");
+        }
+
+        private static void RemoveColors(ArrayList list, ArrayList removeList)
+        {
+
+        }
+    }
+}
